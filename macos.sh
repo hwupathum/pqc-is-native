158c93d21da561fe32cce8639cb3ec60d05a392c
Udara Pathum <46132469+hwupathum@users.noreply.github.com>
2024-03-06 00:30:26 +0530

    Update macos workflow

--- macos.sh
+++ macos.sh
@@ -1,56 +1,35 @@
 #! /bin/bash
 
-OPENSSL_VERSION=3.2.1
-APR_VERSION=1.7.4
-TCNATIVE_VERSION=1.3.0
+# OPENSSL_VERSION=3.2.1
+# APR_VERSION=1.7.4
+# TCNATIVE_VERSION=1.3.0
 
-GITHUB_WORKSPACE=$(pwd)
-OPENSSL_INSTALL=$GITHUB_WORKSPACE/build/openssl
-APR_INSTALL=$GITHUB_WORKSPACE/build/apr
-OUTPUT_DIR=$GITHUB_WORKSPACE/lib
+# GITHUB_WORKSPACE=$(pwd)
+# OPENSSL_INSTALL=$GITHUB_WORKSPACE/build/openssl
+# APR_INSTALL=$GITHUB_WORKSPACE/build/apr
+# OUTPUT_DIR=$GITHUB_WORKSPACE/lib
+
+# export OPENSSL_INSTALL=$OPENSSL_INSTALL
 
 # ----------- Build static version of OpenSSL 3.x.x -----------
 
 mkdir -p $OUTPUT_DIR
 
-if [ -f openssl-$OPENSSL_VERSION.tar.gz ]; then
-    echo "File exists"
-else
-    echo "Downloading openssl-$OPENSSL_VERSION source code..."
-    wget https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
-fi
-rm -rf openssl-$OPENSSL_VERSION
-tar -xvf openssl-$OPENSSL_VERSION.tar.gz
+echo "Downloading openssl-$OPENSSL_VERSION source code..."
+wget https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
+tar -xf openssl-$OPENSSL_VERSION.tar.gz
 cd openssl-$OPENSSL_VERSION
 
 ./Configure --prefix=$OPENSSL_INSTALL  no-shared -fPIC --openssldir=$OUTPUT_DIR
 make && make install_sw
 
-# ----------- Install ops provider for OpenSSL -----------
-
-export OPENSSL_INSTALL=$OPENSSL_INSTALL
-cd $GITHUB_WORKSPACE
-rm -rf oqs-provider
-git clone -b main https://github.com/open-quantum-safe/oqs-provider.git
-cd oqs-provider
-
-./scripts/fullbuild.sh
-cmake --install _build
-
-cp $OPENSSL_INSTALL/lib/ossl-modules/oqsprovider.dylib $OUTPUT_DIR
-
 
 # ----------- Build static version of APR 1.6.3 or later -----------
 
 cd $GITHUB_WORKSPACE
-if [ -f apr-$APR_VERSION.tar.gz ]; then
-    echo "File exists"
-else
-    echo "Downloading apr-$APR_VERSION source code..."
-    wget https://dlcdn.apache.org//apr/apr-$APR_VERSION.tar.gz
-fi
-rm -rf apr-$APR_VERSION
-tar -xvf apr-$APR_VERSION.tar.gz
+echo "Downloading apr-$APR_VERSION source code..."
+wget https://dlcdn.apache.org//apr/apr-$APR_VERSION.tar.gz
+tar -xf apr-$APR_VERSION.tar.gz
 cd apr-$APR_VERSION
 
 # See https://stackoverflow.com/questions/18091991/error-while-compiling-apache-apr-make-file-not-found
@@ -60,20 +39,38 @@ make && make install
 
 # Ensure the static APR library is used
 apr_file_path="$APR_INSTALL/lib/libapr-1.la"
-sed 's/^dlname=/#&/' -i $apr_file_path
-sed 's/^library_names=/#&/' -i $apr_file_path
+if [ -e "$apr_file_path" ]; then
+    # Backup the file
+    cp "$apr_file_path" "$apr_file_path.bak"
+
+    # Comment or delete the specified sections using awk
+    awk '/dlname=/ {$0="#"$0} /library_names=/ {$0="#"$0} {print}' "$apr_file_path" > "$apr_file_path.temp"
+    mv "$apr_file_path.temp" "$apr_file_path"
+
+    echo "Sections in libapr-1.la edited successfully."
+else
+    echo "Error: libapr-1.la file not found in $APR_INSTALL/lib."
+fi
 
 # ----------- Build tc-native -----------
 
 cd $GITHUB_WORKSPACE
-if [ -f tomcat-native-$TCNATIVE_VERSION-src.tar.gz ]; then
-    echo "File exists"
-else
-    echo "Downloading tomcat-native-$TCNATIVE_VERSION source code..."
-    wget https://dlcdn.apache.org/tomcat/tomcat-connectors/native/$TCNATIVE_VERSION/source/tomcat-native-$TCNATIVE_VERSION-src.tar.gz
-fi
+echo "Downloading tomcat-native-$TCNATIVE_VERSION source code..."
+wget https://dlcdn.apache.org/tomcat/tomcat-connectors/native/$TCNATIVE_VERSION/source/tomcat-native-$TCNATIVE_VERSION-src.tar.gz
 
-tar -xvf tomcat-native-$TCNATIVE_VERSION-src.tar.gz
+tar -xf tomcat-native-$TCNATIVE_VERSION-src.tar.gz
 cd tomcat-native-$TCNATIVE_VERSION-src/native
 ./configure --with-apr=$APR_INSTALL --with-ssl=$OPENSSL_INSTALL --prefix=$GITHUB_WORKSPACE
 make && make install
+
+# ----------- Install ops provider for OpenSSL -----------
+
+export OPENSSL_INSTALL=$OPENSSL_INSTALL
+cd $GITHUB_WORKSPACE
+git clone -b main https://github.com/open-quantum-safe/oqs-provider.git
+cd oqs-provider
+
+./scripts/fullbuild.sh
+cmake --install _build
+
+cp $OPENSSL_INSTALL/lib/ossl-modules/oqsprovider.dylib $OUTPUT_DIR
